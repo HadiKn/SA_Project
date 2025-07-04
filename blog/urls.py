a4206db0852c186e363545c0800a17eefd000e44
inannaaaa <inanajabbour@gmail.com>
2025-07-04 15:45:42 +0300

    made comments model and serializers

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/posts/' , include('posts.urls')),
-    path('api/users/' , include('users.urls')),
+    path('api/posts/', include('posts.urls')),
+    path('api/comments/', include('comments.urls')),  
+    path('api/likes/', include('likes.urls')),  
+    path('api/users/', include('users.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
